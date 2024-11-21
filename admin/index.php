fa869ce204751df7155033056b28a8bb9be44ee8
Doduy2110 <duydvph46536@fpt.edu.vn>
2024-11-21 22:55:25 +0700

    base dự án

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php 
+  session_start();
+    require_once '../commons/env.php';
+    require_once '../commons/function.php.php';
+
+    //require controller
+
+    require_once './controllers/AdminBaoCaoThongKeController.php';
+    require_once './controllers/AdminDanhMucController.php';
+    require_once './controllers/AdminDonHangController.php';
+    require_once './controllers/AdminTaiKhoanController.php';
+    require_once './controllers/AdminSanPhamController.php';
+    //require models
+
+    require_once './models/AdminDanhMuc.php';
+    require_once './models/AdminDonHang.php';
+    require_once './models/AdminSanPham.php';
+    require_once './models/AdminTaiKhoan.php';
+    require_once './models/ThongKe.php';
+
+
+    //route 
+    $act = $_GET['act']??'/';
+    //checklogin admin
+
+
+
+    match($act){
+        //Định nghĩa các route
+        
+
+    };
