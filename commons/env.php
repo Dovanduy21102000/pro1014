05a4476a49caa5972913491c9580552534a49181
duc0509 <ducpvph50953@gmail.com>
2024-11-28 11:06:50 +0700

    donhang admin 28/11

--- commons/env.php
+++ commons/env.php
@@ -3,13 +3,13 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/pro1014/petshop/');
-define('BASE_URL_ADMIN'       , 'http://localhost/pro1014/petshop/admin');
+define('BASE_URL'       , 'http://localhost/pro1014/');
+define('BASE_URL_ADMIN'       , 'http://localhost/pro1014/admin/?act=');
 
 define('DB_HOST'    , 'localhost');
-define('DB_PORT'    , 3307);
+define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'hanhdzso1tg');
-define('DB_NAME'    , 'duan1_pro1014');  // Tên database
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'petshop');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
